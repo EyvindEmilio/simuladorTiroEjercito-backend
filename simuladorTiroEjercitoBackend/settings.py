2233dbefafa84a0bf54ad46f3df6ab9df9b54664
EyvindEmilio <eyvind_coaquira@hotmail.com>
2015-11-26 01:15:55 -0400

    config session auth

--- simuladorTiroEjercitoBackend/settings.py
+++ simuladorTiroEjercitoBackend/settings.py
@@ -120,6 +120,8 @@ AUTH_USER_MODEL = 'simulador.Account'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         # 'oauth2_provider.ext.rest_framework.OAuth2Authentication',
         # 'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
