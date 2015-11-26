ce904da1cc65a99648a7876f0663cd1a79339571
EyvindEmilio <eyvind_coaquira@hotmail.com>
2015-11-26 00:29:54 -0400

    settings

--- simuladorTiroEjercitoBackend/urls.py
+++ simuladorTiroEjercitoBackend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from simulador.resources.account import LoginView, AccountViewSet
 from simulador.resources.account import LogoutView
