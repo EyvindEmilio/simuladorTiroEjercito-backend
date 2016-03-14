febc3eecaebc945aef0225abb751e85e6c34341b
EyvindEmilio <eyvind_coaquira@hotmail.com>
2016-03-14 04:26:20 +0100

    Direccion de frontend

--- simuladorTiroEjercitoBackend/settings.py
+++ simuladorTiroEjercitoBackend/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from distutils import dirname
-from os.path import abspath, basename, normpath, join
+from os.path import join
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
